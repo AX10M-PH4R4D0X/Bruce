920bd5395e774d6c3507891e14ddd5e4e8458a05
AXI0M <dvannoc047@gmail.com>
2026-08-14 22:13:59 +0300

    Update password strength calculator to V.0.2

--- Bruce.c
+++ Bruce.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
+
 #include <stdlib.h>
+
 #include <string.h>
+
 #include <ctype.h>
+
 #include <math.h>
+
+
 #include "CharSet.h"
 #define MAX_LEN 31
+
 
 
 typedef struct {
@@ -26,6 +33,7 @@ typedef struct {
 }Crack_Time;
 
 
+
 int Calculate_CharSet_Size(int HasNum, int HasUpper, int HasLower, int HasSymbol);
 
 long double Combinations(int CharSet, int Len_Of_Password);
@@ -43,9 +51,10 @@ int main () {
     long double Combination_Count;
 
 
-    puts("\n\nBruce| Password Strength Calculator | V.0.1\n**Note: Maximum password length is \"30 characters\" any extra characters will be ignored.\n");
+    puts("\n\nBruce| Password Strength Calculator | V.0.2\n**Note: Maximum password length is \"30 characters\" any extra characters will be ignored.\n");
 
-    printf("\n\n------------------------------\n1-[Calculate Combinations]\n\n2-[Calculate Crack Time]\n\n3-[Calculate All & Print]\n\n4-[EXIT]\n------------------------------\n\n");
+    printf("\n\n------------------------------\n1-[Calculate Crack Time]\n\n2-[Calculate Calculate Crack Time and Combination Count]\n\n3-[EXIT]\n------------------------------\n\n");
+    
     scanf(" %c",&choice);
     getchar();
 
@@ -63,12 +72,12 @@ int main () {
 
 
         Len_Of_Password = strlen(Password);
+
         
-    
         for(int i = 0; i < Len_Of_Password; i++) {
-
+        
             char character = Password[i];
-
+        
             if (isdigit(character)) {
 
                 HasNum = 1;
@@ -86,22 +95,48 @@ int main () {
                 HasSymbol = 1;
 
             }
-            
-            
+    
         }
-
+    
 
         CharSet = Calculate_CharSet_Size(HasNum, HasUpper, HasLower, HasSymbol);
-
+    
         Combination_Count = Combinations(CharSet, Len_Of_Password);
 
+        ct = Estimated_Crack_Time(Combination_Count); 
 
-        printf("\nYour Password ---> [%s]\nCombination Count ---> [%.0Lf]\n\n",Password,Combination_Count);
+
+
+        printf("\nYour Password ---> [%s]\n\nEstimated Crack Time **Under 1 trillion attempts per second**\n[%.0Lf Years %.0Lf Months %.0Lf Weeks %.0Lf Days %.0Lf Hours %.0Lf Mins %.0Lf Secs]\n\n",Password, ct.years, ct.months, ct.weeks, ct.days, ct.hours, ct.mins, ct.secs);
+
+
+        if (ct.years >= 0.0L && ct.years <= 50.0L) {
+            
+            puts("Change your password and Enable Multi-Factor Authentication Immediately it's too weak!\n\n");
+
+        } else if (ct.years > 50.0L && ct.years <= 100.0L) {
+            
+            puts("This is a moderately strong password.\nDepending on the risks, it is recommended to change it within 1 to 3 months and 'Always Enable' Multi-Factor Authentication!\n\n");
+
+        } else if (ct.years > 100.0L && ct.years <= 500.0L) {
+
+            puts("This is a strong password.\nBut do not neglect periodic password changes for example 6 or 8 months and 'Always Enable' Multi-Factor Authentication!\n\n");
+
+        } else if (ct.years > 500.0L && ct.years <= 1000.0L) {
+
+            puts("This is a very strong password.\nHowever, do not neglect periodic password changes for example twice per year and 'Always Enable' Multi-Factor Authentication!\n\n");
+
+        } else {
+
+            puts("This is an extremely strong password.\nHowever, do not neglect periodic password changes for example once per year and 'Always Enable' Multi-Factor Authentication!\n\n");
+
+        }
+
         break;
-    
+        
 
         case '2':
-    
+
         printf("Please Enter Your Password:");
         fgets(Password, MAX_LEN, stdin);
 
@@ -110,11 +145,11 @@ int main () {
 
         Len_Of_Password = strlen(Password);
 
-        
+
         for(int i = 0; i < Len_Of_Password; i++) {
-        
+
             char character = Password[i];
-        
+
             if (isdigit(character)) {
 
                 HasNum = 1;
@@ -132,9 +167,9 @@ int main () {
                 HasSymbol = 1;
 
             }
-    
+
         }
-    
+
 
         CharSet = Calculate_CharSet_Size(HasNum, HasUpper, HasLower, HasSymbol);
     
@@ -143,57 +178,35 @@ int main () {
         ct = Estimated_Crack_Time(Combination_Count); 
 
 
-        printf("\nYour Password ---> [%s]\n\nEstimated Crack Time\n[%.0Lf Years %.0Lf Months %.0Lf Weeks %.0Lf Days %.0Lf Hours %.0Lf Mins %.0Lf Secs]\n\n",Password, ct.years, ct.months, ct.weeks, ct.days, ct.hours, ct.mins, ct.secs);
+        printf("\nYour Password ---> [%s]\nCombination Count ---> [%.0Lf]\n\nEstimated Crack Time **Under 1 trillion attempts per second**\n[%.0Lf Years %.0Lf Months %.0Lf Weeks %.0Lf Days %.0Lf Hours %.0Lf Mins %.0Lf Secs]\n\n",Password, Combination_Count, ct.years, ct.months, ct.weeks, ct.days, ct.hours, ct.mins, ct.secs);
+        
+        
+        if (ct.years >= 0.0L && ct.years <= 50.0L) {
+            
+            puts("Change your password and Enable Multi-Factor Authentication Immediately it's too weak!\n\n");
+
+        } else if (ct.years > 50.0L && ct.years <= 100.0L) {
+            
+            puts("This is a moderately strong password.\nDepending on the risks, it is recommended to change it within 1 to 3 months and 'Always Enable' Multi-Factor Authentication!\n\n");
+
+        } else if (ct.years > 100.0L && ct.years <= 500.0L) {
+
+            puts("This is a strong password.\nBut do not neglect periodic password changes for example 6 or 8 months and 'Always Enable' Multi-Factor Authentication!\n\n");
+
+        } else if (ct.years > 500.0L && ct.years <= 1000.0L) {
+
+            puts("This is a very strong password.\nHowever, do not neglect periodic password changes for example twice per year and 'Always Enable' Multi-Factor Authentication!\n\n");
+
+        } else {
+
+            puts("This is an extremely strong password.\nHowever, do not neglect periodic password changes for example once per year and 'Always Enable' Multi-Factor Authentication!\n\n");
+
+        }
+
         break;
+        
         
         case '3':
-
-        printf("Please Enter Your Password:");
-        fgets(Password, MAX_LEN, stdin);
-
-        Password[strcspn(Password,"\n")] = 0;
-
-
-        Len_Of_Password = strlen(Password);
-
-
-        for(int i = 0; i < Len_Of_Password; i++) {
-
-            char character = Password[i];
-
-            if (isdigit(character)) {
-
-                HasNum = 1;
-
-            } else if (isupper(character)) {
-                
-                HasUpper = 1;
-
-            } else if (islower(character)) {
-                
-                HasLower = 1;
-
-            } else {
-                
-                HasSymbol = 1;
-
-            }
-
-        }
-
-
-        CharSet = Calculate_CharSet_Size(HasNum, HasUpper, HasLower, HasSymbol);
-    
-        Combination_Count = Combinations(CharSet, Len_Of_Password);
-
-        ct = Estimated_Crack_Time(Combination_Count); 
-
-
-        printf("\nYour Password ---> [%s]\nCombination Count ---> [%.0Lf]\n\nEstimated Crack Time\n[%.0Lf Years %.0Lf Months %.0Lf Weeks %.0Lf Days %.0Lf Hours %.0Lf Mins %.0Lf Secs]\n\n",Password, Combination_Count, ct.years, ct.months, ct.weeks, ct.days, ct.hours, ct.mins, ct.secs);
-        break;
-        
-        
-        case '4':
         
         puts("[Good-Bye!]\n");
         break;
@@ -211,6 +224,10 @@ int main () {
 }
 
 
+
+
+
+//Flags are being read and Character Set Size is being calculated
 int Calculate_CharSet_Size(int HasNum, int HasUpper, int HasLower, int HasSymbol) {
 
     int CharSet_Size = 0;
@@ -228,6 +245,8 @@ int Calculate_CharSet_Size(int HasNum, int HasUpper, int HasLower, int HasSymbol
 }
 
 
+
+//The number of combinations in the password is calculated
 long double Combinations(int CharSet, int Len_Of_Password) {
 
     long double Find_Combinations = pow((long double)CharSet, Len_Of_Password);
@@ -237,14 +256,19 @@ long double Combinations(int CharSet, int Len_Of_Password) {
 }
 
 
+
+//The estimated cracking time of the password is calculated under 1 trillion attempts per second and assigned to a variable after converting it into a format that people can read.
 Crack_Time Estimated_Crack_Time(long double Combination_Count) {
 
+    //Struct Variable Declaration 
     Crack_Time result;
 
+    //Calculation
     const long double Attempts_per_sec = 1000000000000.0;
     long double Crack_Time = (Combination_Count/ Attempts_per_sec);
 
 
+    //Converting And Struct Initialization
     result.years = floor(Crack_Time / 31536000);
     Crack_Time = fmod(Crack_Time, 31536000);
 
@@ -263,6 +287,7 @@ Crack_Time Estimated_Crack_Time(long double Combination_Count) {
     result.mins = floor(Crack_Time / 60);
 
     result.secs = fmod(Crack_Time, 60);
+
 
     return result;
 
